4c939a4541742364cfe0313ecd7c482d016d5685
davlatberdinazarov <aberdinazarov71@gmail.com>
2024-11-11 20:30:16 +0500

    base url modified

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = `http://localhost:8181`;
+export const BASE_URL = `http://185.219.81.112:8181`;
 
 export const $axios = axios.create({
     baseURL: `${BASE_URL}/api`,
